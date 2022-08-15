022c8589c2e9de1727068b59b73bbc7b604985f1
Ashfaq Hossain <ashfaq.hossain@g.bracu.ac.bd>
2022-08-15 11:25:32 +0600

    update models by adding user_type

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,30 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password", "user_type")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
 
 
 
