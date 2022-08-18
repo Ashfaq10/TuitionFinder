606e113cba5d8df52192f5d57f36cb135129b1ee
Ashfaq Hossain <ashfaq.hossain@g.bracu.ac.bd>
2022-08-18 17:51:57 +0600

    update models by adding user_type

--- tuition/urls.py
+++ tuition/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from tuition.views import HomeView
+from tuition.views import HomeView, TuitionDetailView, TuitorDetailView, TuitionAdd, TuitionDelete
 
 app_name = 'tuition'
 
 urlpatterns = [
-    path("", HomeView.as_view(), name = 'homepage')
+
+    path("", HomeView.as_view(), name = 'homepage'),
+    path("tuition-details/<int:id>/", TuitionDetailView.as_view(), name = 'tuition_detail'),
+    path("tuitor-details/<int:id>/", TuitorDetailView.as_view(), name = 'tuitor_detail'),
+    path("tuition-add/", TuitionAdd.as_view(), name = 'tuition_add'),
+    path("tuition-delete/<int:id>/", TuitionDelete.as_view(), name = 'tuition_delete'),
     
+  
 ]
--- tuition/views.py
+++ tuition/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render
 from django.views import generic
 
+
 # Create your views here.
 
 class HomeView(generic.View):
     template_name = "tuition/home.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+class TuitionDetailView(generic.View):
+    template_name = "tuition/tuition_detail.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+class TuitorDetailView(generic.View):
+    template_name = "tuition/tuitor_detail.html"
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+class TuitionAdd(generic.View):
+    template_name = "tuition/tuition_add.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+
+class TuitionDelete(generic.View):
+    template_name = "tuition/tuition_delete.html"
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
